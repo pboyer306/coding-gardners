8bf549404985c9f00e697369e7f63adbf88823dd
Julia Young <young.juliamay@gmail.com>
2024-07-25 19:43:38 -0600

    Updated files

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const plants = [
-    
-    {
+const plants = [{
     name: "Zucchini",
     zones: [2,3,4,5,6,7,8,9,10,11],
     IdealPlantingMonth: "May",
@@ -9,53 +7,46 @@ const plants = [
      SunExposure: "8-10 hours",
      MoreInfo: "https://www.almanac.com/plant/zucchini",
 
-    },
-
-   { 
-    name: "Yellow Squash",
+},
+   { name: "Yellowsquash",
     zones: [3,4,5,6,7,8,9,10,11],
     IdealPlantingMonth: "March",
     DaytoMaturity: "35-110 days",
     AnnualPerrenial: "Annual",
-    SunExposure: "6+ hours",
-    MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-squash",
+     SunExposure: "6+ hours",
+     MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-squash",
 
-    },
-
-    {
+},
+{
     name: "Pumpkin",
     zones: [3,4,5,6,7,8,9],
     IdealPlantingMonth: "May/June",
     DaytoMaturity: "90-120 days",
     AnnualPerrenial: "Annual",
-    SunExposure: "6+ hours",
-    MoreInfo: "https://www.thespruce.com/how-to-grow-pumpkins-1403469#:~:text=Plant%20pumpkins%20in%20early%20spring,hill%2C%20about%201%20inch%20deep.",
+     SunExposure: "6+ hours",
+     MoreInfo: "https://www.thespruce.com/how-to-grow-pumpkins-1403469#:~:text=Plant%20pumpkins%20in%20early%20spring,hill%2C%20about%201%20inch%20deep.",
 
-    },
-
+},
     {
     name: "Cucumber",
     zones: [4,5,6,7,8,9,10,11,12],
     IdealPlantingMonth: "May",
     DaytoMaturity: "60 days",
     AnnualPerrenial: "Annual",
-    SunExposure: "6-8 hours",
-    MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-cucumbers",
+     SunExposure: "6-8 hours",
+     MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-cucumbers",
 
-    },
-
-    {
-    name: "Cabbage",
+},
+    {name: "Cabbage",
     zones: [2,3,4,5,6,7,8,9],
     IdealPlantingMonth: "April",
     DaytoMaturity: "100-120 days",
     AnnualPerrenial: "Annual",
-    SunExposure: "6-8 hours",
-    MoreInfo: "https://extension.usu.edu/yardandgarden/research/cabbage-in-the-garden",
+     SunExposure: "6-8 hours",
+     MoreInfo: "https://extension.usu.edu/yardandgarden/research/cabbage-in-the-garden",
 
-    },
-
-    {
+},
+ {
     name: "Eggplant",
     zones: [2,3,4,8],
     IdealPlantingMonth: "March",
@@ -64,9 +55,8 @@ const plants = [
      SunExposure: "6-9 hours",
      MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-eggplant",
 
-    },
-
-    {
+},
+ {
     name: "Strawberry",
     zones: [4,6,7],
     IdealPlantingMonth: "April",
@@ -74,9 +64,9 @@ const plants = [
     AnnualPerrenial: "Perennial",
     SunExposure: "8 hours",
     MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-strawberries",
-    },
+},
 
-    {
+ {
     name: "Blueberry",
     zones: [3,5,8,9],
     IdealPlantingMonth: "April",
@@ -85,9 +75,9 @@ const plants = [
      SunExposure: "7-9 hours",
      MoreInfo: "https://extension.umn.edu/fruit/growing-blueberries-home-garden",
 
-    },
+},
 
-    {
+{
     name: "Cilantro",
     zones: [2,5,6],
     IdealPlantingMonth: "May",
@@ -96,9 +86,9 @@ const plants = [
      SunExposure: "6-8 hours",
      MoreInfo: "https://bonnieplants.com/blogs/how-to-grow/growing-cilantro",
 
-    },
+},
 
-    {
+ {
     name: "Basil",
     zones: [3,4],
     IdealPlantingMonth: "March",
@@ -107,7 +97,7 @@ const plants = [
      SunExposure: "7 hours",
      MoreInfo: "https://www.almanac.com/plant/basil",
 
-    },
+},
 
 {
     name: "Raspberry",
@@ -196,48 +186,85 @@ const plants = [
 
 }];
 
-const zoneselect = document.getElementById('zoneSelect');
-const plantselect = document.getElementById('plantSelect');
-zoneselect.addEventListener('change', function(event){
-let plantName = '';
-const plantNameArray = [];
-const chosenzone = plants.filter((name) => name.zones.includes(Number(event.target.value)))
-for (i=0; i < chosenzone.length; i++){
-    plantName = chosenzone[i].name
-    plantNameArray.push(plantName);
-  };
-plantselect.innerHTML = '';
-for(let i=0; i<plantNameArray.length; i++) { 
-    const option = document.createElement('option');
-        option.textContent = plantNameArray[i];
-        plantselect.appendChild(option);
-        };
-});
-
-const nameOfPlant = document.getElementById('plantName');
-const plantingMonth = document.getElementById('plantingMonth');
-const daysToMaturity = document.getElementById('daysToMaturity');
-const annualOrPer = document.getElementById('annualOrPer');
-const sunExposure = document.getElementById('sunExposure');
-const moreInfoLink = document.getElementById('moreInfoLink');
-
-
-const submit = document.getElementById('submit');
-submit.addEventListener('click', function(event){
-    const selectedPlantName = plantselect.value;
-    const selectedPlantObject = plants.find(plant => plant.name === selectedPlantName);
-    if (selectedPlantObject) {
-        plantName.innerHTML = "Plant Name: " + selectedPlantObject.name;
-        plantingMonth.innerHTML = "Ideal Planting Month: " + selectedPlantObject.IdealPlantingMonth;
-        daysToMaturity.innerHTML = "Days to Maturity: " + selectedPlantObject.DaytoMaturity;
-        annualOrPer.innerHTML = "Annual or Perennial: " + selectedPlantObject.AnnualPerrenial;
-        sunExposure.innerHTML = "Sun Exposure: " + selectedPlantObject.SunExposure;
-        
-        // Clear the existing moreInfoLink content before adding the new link
-        moreInfoLink.innerHTML = "More Info: ";
-        const anchorElement = document.createElement('a');
-        anchorElement.href = selectedPlantObject.MoreInfo;
-        anchorElement.textContent = "Click here!"; 
-        moreInfoLink.appendChild(anchorElement);
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("myModal");
+    const closeBtn = document.getElementsByClassName("close")[0];
+  
+    modal.style.display = "block";
+  
+    closeBtn.onclick = function() {
+      modal.style.display = "none";
     }
-});
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+
+
+  const zoneselect = document.getElementById('zoneSelect');
+  const plantselect = document.getElementById('plantSelect');
+  zoneselect.addEventListener('change', function(event){
+  let plantName = '';
+  const plantNameArray = [];
+  const chosenzone = plants.filter((name) => name.zones.includes(Number(event.target.value)))
+  for (i=0; i < chosenzone.length; i++){
+      plantName = chosenzone[i].name
+      plantNameArray.push(plantName);
+    };
+  plantselect.innerHTML = '';
+  for(let i=0; i<plantNameArray.length; i++) { 
+      const option = document.createElement('option');
+          option.textContent = plantNameArray[i];
+          plantselect.appendChild(option);
+          };
+  });
+  
+  const nameOfPlant = document.getElementById('plantName');
+  const plantingMonth = document.getElementById('plantingMonth');
+  const daysToMaturity = document.getElementById('daysToMaturity');
+  const annualOrPer = document.getElementById('annualOrPer');
+  const sunExposure = document.getElementById('sunExposure');
+  const moreInfoLink = document.getElementById('moreInfoLink');
+  
+  
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', function(event){
+      const selectedPlantName = plantselect.value;
+      const selectedPlantObject = plants.find(plant => plant.name === selectedPlantName);
+      if (selectedPlantObject) {
+          plantName.innerHTML = "Plant Name: " + selectedPlantObject.name;
+          plantingMonth.innerHTML = "Ideal Planting Month: " + selectedPlantObject.IdealPlantingMonth;
+          daysToMaturity.innerHTML = "Days to Maturity: " + selectedPlantObject.DaytoMaturity;
+          annualOrPer.innerHTML = "Annual or Perennial: " + selectedPlantObject.AnnualPerrenial;
+          sunExposure.innerHTML = "Sun Exposure: " + selectedPlantObject.SunExposure;
+          moreInfoLink.innerHTML = "More Info: ";
+          const anchorElement = document.createElement('a');
+          anchorElement.href = selectedPlantObject.MoreInfo;
+          anchorElement.textContent = "Click here!"; 
+          moreInfoLink.appendChild(anchorElement);
+          let recentlySearchedPlants = JSON.parse(localStorage.getItem('recentlySearchedPlants')) || [];
+          recentlySearchedPlants.push(selectedPlantObject);
+              if (recentlySearchedPlants.length > 3) {
+                  recentlySearchedPlants = recentlySearchedPlants.slice(recentlySearchedPlants.length - 1);
+              }
+          localStorage.setItem('recentlySearchedPlants', JSON.stringify(recentlySearchedPlants));
+          const recentlySearched = document.getElementById('recentlySearched');
+          recentlySearched.innerHTML = '';
+          recentlySearchedPlants.forEach((plant, index) => {
+              recentlySearched.innerHTML += `
+                  <div>
+                      <li>Plant Name: ${plant.name}</li>
+                      <li>Ideal Planting Month: ${plant.IdealPlantingMonth}</li>
+                      <li>Days to Maturity: ${plant.DaytoMaturity}</li>
+                      <li>Annual or Perennial: ${plant.AnnualPerrenial}</li>
+                      <li>Sun Exposure: ${plant.SunExposure}</li>
+                      <p></p>
+                  </div>
+              `;
+          });
+      }
+  });
+
